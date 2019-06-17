b0cbdc250b47de39839c3319a68b3d8e67546c4e
Jelle Monen <jelle.monen@hotmail.com>
2019-06-17 11:55:29 +0200

    added superagent

---
+++ src/components/AlbumsList.js
@@ -0,0 +1,10 @@
+import * as React from 'react'
+
+export default function AlbumsList(props) {
+  return (<div>
+    <h1>All Albums</h1>
+
+    There are { props.albums.length } albums available.
+    {console.log(this.props.albums)}
+  </div>)
+}
