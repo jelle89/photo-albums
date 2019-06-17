ebe1acb584fc3ccaa62602fbe143421918f62067
Jelle Monen <jelle.monen@hotmail.com>
2019-06-17 18:45:02 +0200

    update voor thuis

--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -4,7 +4,13 @@ export default function AlbumsList(props) {
   return (<div>
     <h1>All Albums</h1>
 
-    There are { props.albums.length } albums available.
-    {console.log(this.props.albums)}
+    <ul>
+      { props.albums.map(album =>
+        <li key={album.id} onClick={(addAlbum) => props.album.id}>
+          { album.title }
+        </li>
+      ) }
+    </ul>
+
   </div>)
 }
--- src/components/AlbumsListContainer.js
+++ src/components/AlbumsListContainer.js
@@ -1,17 +1,51 @@
 import * as React from 'react'
 import * as request from 'superagent'
 import AlbumsList from './AlbumsList'
+import { connect } from 'react-redux'
+import { addAlbum } from '../actions/test'
 
-export default class AlbumsListContainer extends React.Component {
+const sleep = time => new Promise(
+  resolve => setTimeout(() => resolve(`I waited for ${time} ms`), time)
+)
+
+class AlbumsListContainer extends React.Component {
   state = {}
 
   componentDidMount() {
     request('https://jsonplaceholder.typicode.com/albums')
       .then(response => this.setState({ albums: response.body }))
+      sleep(2000)
+        .then(message => this.props.addAlbum(5, 'Enjoying Sunshine'))
+      sleep(3000)
+        .then(message => this.props.addAlbum(10, 'Having fun in the US'))
+      
   }
 
+  addAlbum = (id, title) => {
+    this.props.dispatch({
+      type: 'ADD_ALBUM',
+      payload: {
+        id: id,
+        title: title
+      }
+    })
+    return addAlbum
+  }
+  
+  
+
   render() {
+    
     if (!this.state.albums) return 'Loading...'
-    return <AlbumsList albums={this.state.albums} />
+    return <AlbumsList albums={this.props.albums}  /> 
+    
   }
 }
+
+const mapStateToProps = (state) => {
+  return {
+    albums: state.albums
+  }
+}
+
+export default connect(mapStateToProps, { addAlbum: addAlbum })(AlbumsListContainer)
